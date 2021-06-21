c9e4c66cbc7c0a80c53bb17c32b7d320382d5b77
philipotieno <iamphilipotieno@gmail.com>
2021-06-21 18:57:13 +0300

    feat: add useeffect to get the story IDs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
