fcf9893b2351a2a632b783c3c819b88b1d0f1e15
philipotieno <iamphilipotieno@gmail.com>
2021-07-07 07:23:20 +0300

    feat: Adding title to stories container

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -8,5 +8,12 @@ export const StoriesContainer = () => {
     getStoryIds().then((data) => setStoryIds(data));
     getStory(20970623).then((data) => console.log(data));
   }, []);
-  return storyIds.map((storyId) => <Story key={storyId} storyId={storyId} />);
+  return (
+    <>
+      <h1>Hacker News Stories</h1>
+      {storyIds.map((storyId) => (
+        <Story key={storyId} storyId={storyId} />
+      ))}
+    </>
+  );
 };
---
+++ src/styles/StoryStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StoryWrapper = styled.section`
+  padding-top: 10px;
+  margin-bottom: 20px;
+  border-top: 1px solid #cccccc;
+
+  &:first-of-type {
+    border-top: 0;
+  }
+
+  &:last-of-type {
+    margin-bottom: 0;
+    padding-bottom: 0; 
+  }
+`;
