bede62ba85307373882ee39aedf747960cc09c17
philipotieno <iamphilipotieno@gmail.com>
2021-07-07 06:22:11 +0300

    feat: demonstrating the results we get from the HN api

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { getStoryIds } from '../services/hnApi';
+import { getStoryIds, getStory } from '../services/hnApi';
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
   useEffect(() => {
     getStoryIds().then((data) => setStoryIds(data));
+    getStory(20970623).then((data) => console.log(data));
   }, []);
   return <p>{storyIds}</p>;
 };
